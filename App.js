fd3e5121e497cc2cb05a8b90eb878976c36b2824
July Muñoz <julita_056@hotmail.com>
2021-01-31 09:14:38 -0500

    ADD: create the project and add some styles

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
 import cardsJSON from './data.json';
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   flexWrap: 'wrap',
-   paddingTop: 22,
-   justifyContent: 'center',
-   alignItems: 'center',
-   height: 250,
-   backgroundColor: '#eff7e1',
-   margin:15 
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: '#fff3e6',
+    flex: 1,
+    flexWrap: 'wrap',
+    paddingTop: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 'auto',
+    backgroundColor: '#fff3e6',
+    margin:15 
   },
   item: {
     padding: 10,
@@ -19,13 +22,26 @@ const styles = StyleSheet.create({
     height: 44,
   },
   image: {
-    alignItems: 'flex-start',
-    height:100,
-    width:100,
-    marginBottom:15,
+    height:250,
+    width:250,
+    marginBottom:15
+  },
+  title: {
+    color:'#222831',
+    fontSize:16,
+    fontWeight: 'bold'
   },
   text: {
-    fontSize:20,
+    color: '#393e46',
+    fontSize:12,
+    paddingTop:5
+  },
+  fixToText: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+    width: 250
   },
 });
 
@@ -47,9 +63,38 @@ class Cinema extends Component {
         <View  key={index} 
         style={styles.container}>
           <Image style={styles.image} source={{uri: card.image}}/>
+          {/* texto */}
+          <Text style={styles.title}>
+            {card.title}
+          </Text>
           <Text style={styles.text}>
-            {card.title} {card.genre} {card.price} {card.inventory} {card.date}
-          </Text>      
+            {card.genre}
+          </Text>
+          <Text style={styles.text}>
+            {card.price}
+          </Text>
+          <Text style={styles.text}>
+            {card.inventory}
+          </Text>
+          <Text style={styles.text}>
+            {card.date}
+          </Text>  
+          {/* Boton */}
+          <View style={styles.fixToText}>
+            <Button 
+              color='#c1a1d3'
+              width= '100'
+              title="Me gusta"
+              onPress={() => Alert.alert('Left button pressed')}
+            />
+            <Button
+              borderRadius='20'
+              color='#1a508b'
+              title="Comprar"
+              onPress={() => Alert.alert('Right button pressed')}
+            />
+          </View>
+          
         </View>
         )
       })
